4a35adecfba975232b0166c5a56351b5439dd322
Ben W <merovex@hey.com>
2021-12-27 09:09:36 -0500

    add description to terradoma map

--- content/tools/terradoma-map/terradoma-map.js
+++ content/tools/terradoma-map/terradoma-map.js
@@ -4,27 +4,68 @@ $.getJSON("terradoma-volume-data.json", function (data) {
   volumes = data["volumes"];
 });
 
-function input(id) { return document.getElementById(id).value }
-function showVolumeDetails() {
-  var key = input('coordinate');
-  if (key.length == 4) {
-    var data = volumes[key];
-    console.log(data)
-    document.getElementById("bases").innerHTML = data["bases"]
-    document.getElementById("factions").innerHTML = data["factions"]
-    document.getElementById("location").innerHTML = data["location"]
-    document.getElementById("name").innerHTML = data["name"]
-    document.getElementById("orbits").innerHTML = data["orbits"]
-    document.getElementById("star").innerHTML = data["star"]
-    document.getElementById("temp").innerHTML = data["temp"]
-    document.getElementById("trade_codes").innerHTML = data["trade_codes"]
-    document.getElementById("travel_code").innerHTML = data["travel_code"]
-    document.getElementById("uwp").innerHTML = data["uwp"]
-  }
-  routableVolumes()
+var uData;
+$.getJSON("../uwp-translator/uwp-translator.json", function (data) { uData = data["data"]; });
+
+function getSVal(key) { return $("input[id^='" + key + "']").val(); }
+function setVal(key, value) { $("input[id=" + key + "]").val(value); }
+function input(id) { return $(id).value }
+function translateUWP(sname, uwp) {
+  var bits = uwp.toUpperCase().split('')
+  console.log(uwp, bits)
+  uwpOutput = [];
+  // alert( uwpStarport)
+  uwpOutput.push(sname + " " + uData['port'][bits[0]])    // Starport
+
+  // Planetary Size
+  s = uData['size'][bits[1]]
+  uwpOutput.push("The main world is roughly " + s[1] + " kilometers in diameter, and is <strong>" + s[0] + "</strong>")
+
+  // Atmosphere
+  uwpOutput.push(uData['atmos'][bits[2]])
+
+  // Hydrosphere
+  s = uData['hydro'][bits[3]]
+  uwpOutput.push("The surface is roughly " + s[0] + " percent surface water (or similar fluid), which qualifies it as a <strong>" + s[1] + "</strong> world")
+
+  // Population
+  s = uData['pop'][bits[4]]
+  uwpOutput.push("The main world has a general population of " + s[1] + " local residents, which qualifies it as a <strong>" + s[0] + " population</strong> world")
+
+  // Government
+  s = uData['govt'][bits[5]]
+  uwpOutput.push("The local government is characterized as <strong>" + s[0] + "</strong>, with " + s[1])
+
+  // Law level
+  s = uData['law'][bits[6]]
+  uwpOutput.push("Visitors may find the law " + s[0] + "restrictive as <strong>" + s[1] + "</strong> are regulated or restricted by local authorities")
+  // Tech level
+  s = uData['tech'][bits[8]]
+  uwpOutput.push("Technology is described as " + s + " (See <a href='http://wiki.travellerrpg.com/Tech_Level_Comparison_Chart'>TL chart</a>)")
+
+  $('#uwp-output').html(uwpOutput.join(".\n") + ".")
 }
-function routableVolumes() {
-  var key = input('coordinate');
+function showVolumeDetails() {
+  var key = getSVal('coordinate')
+  if (key.length != 4) { return; }
+
+  var data = volumes[key];
+  console.log(data)
+  $("#bases").html(data["bases"])
+  $("#factions").html(data["factions"])
+  $("#location").html(data["location"])
+  $("#name").html(data["name"])
+  $("#orbits").html(data["orbits"])
+  $("#star").html(data["star"])
+  $("#temp").html(data["temp"])
+  $("#trade_codes").html(data["trade_codes"])
+  $("#travel_code").html(data["travel_code"])
+  $("#uwp").html(data["uwp"])
+
+  routableVolumes(key)
+  translateUWP(data["name"], data["uwp"])
+}
+function routableVolumes(key) {
   if (key.length != 4) { return; }
 
   var routable = ""
@@ -48,10 +89,10 @@ function routableVolumes() {
       if (p > 100) { break; }
     }
   }
-  document.getElementById("routable_volumes").innerHTML = routable;
+  $("#routable_volumes").html(routable);
 }
 function setCoordinate(link) {
   coordinate = link.getAttribute("data-coordinate");
-  document.getElementById('coordinate').value = coordinate
+  setVal('coordinate', coordinate)
   showVolumeDetails()
 }
