dedb32fc0d05155531d3f5e9609fd9f9cb83c717
Ben W <merovex@hey.com>
2022-01-18 08:30:44 -0500

    update wordcount, start progress reports

--- content/tools/writing-progress/writing-progress.js
+++ content/tools/writing-progress/writing-progress.js
@@ -2,41 +2,39 @@ var uData;
 // var cumulative 
 
 const WRITING_DAYS = 6;
-const MAX_RANGE = 365;
-const VALUE_COMPLETION = 75000 * 2 - 24466;
-const PLANNED_COMPLETION = new Date(2022, 5, 30)
-
+const MAX_RANGE = 366;
+const today = new Date();
 var words = {}
+var hours = []
 var earned_value = 0; // Words from the earlier draft
 var days_writing = 0;
-var today = new Date();
+var total_hours = 0;
+
 var start_date = new Date();
 
-$.getJSON("writing-progress.json", function (data) {
-  uData = data["data"];
-  Object.keys(uData).map(function (key) {
-    var date = new Date(key + 'T06:20:30Z')
-    if (date < start_date) { start_date = date; }
+function calculateProgress() {
 
-    if (uData[key]['daily'] > 0 && today.between(date) < MAX_RANGE) {
-      var label = date.getMonday().toISOString().split('T')[0]
-      if (!(label in words)) { words[label] = 0 }
-      words[label] = words[label] + uData[key]['daily'];
-      earned_value = earned_value + uData[key]['daily'];
-      days_writing = days_writing + 1;
-    }
-  })
+  const target_value = document.getElementById('value_completion').value
+  const prior_value = document.getElementById('prior_value').value
+  const VALUE_COMPLETION = target_value - prior_value
+  const PLANNED_COMPLETION = new Date(2022, 5, 30)
+  console.log("Calculating", VALUE_COMPLETION)
 
-  var days_lapsed = today.between(start_date)
+  const days_lapsed = today.between(start_date)
+  const session_average = hours.reduce((a, b) => a + b) / hours.length
 
   // Convert this to Earned Value terminology
-  var planned_value = (VALUE_COMPLETION / PLANNED_COMPLETION.between(start_date)) * today.between(start_date)
-  var schedule_variance = earned_value - planned_value
-  var remaining_work = (VALUE_COMPLETION - earned_value) // How many words to I need to write to finish?
-  var average = earned_value / days_writing; // What is my current daily average?
-  var days_pace = Math.round(remaining_work / average); // At current writing pace, how many days will it take?
-  var target_date = today.addDays(days_pace)// Based on this, when will I likely finish?
-  var days_remaining = PLANNED_COMPLETION.between(today).formatted(); // How many actual days remain?
+  const planned_value = (VALUE_COMPLETION / PLANNED_COMPLETION.between(start_date)) * today.between(start_date)
+  const schedule_variance = earned_value - planned_value
+  const remaining_work = (VALUE_COMPLETION - earned_value) // How many words to I need to write to finish?
+  const average = earned_value / days_writing; // What is my current daily average?
+  const days_pace = Math.round(remaining_work / average); // At current writing pace, how many days will it take?
+  const target_date = today.addDays(days_pace)// Based on this, when will I likely finish?
+  const days_remaining = PLANNED_COMPLETION.between(today).formatted(); // How many actual days remain?
+
+  const words_per_hour = earned_value / total_hours
+
+  const hours_remaining = remaining_work / words_per_hour;
 
   // Am I at risk of missing my date?
   if (PLANNED_COMPLETION < target_date) {
@@ -54,6 +52,10 @@ $.getJSON("writing-progress.json", function (data) {
   document.getElementById("remaining-work").innerHTML = remaining_work.formatted();
   document.getElementById("planned-date").innerHTML = PLANNED_COMPLETION.toString()
   document.getElementById("tracking-date").innerHTML = target_date.toString()
+  document.getElementById("total-hours").innerHTML = total_hours.rounded().toString()
+  document.getElementById("average-hours").innerHTML = session_average.rounded(1).toString()
+  document.getElementById("wph").innerHTML = words_per_hour.rounded().toString();
+  document.getElementById("hours-remaining").innerHTML = hours_remaining.rounded().toString();
 
   // Writing Pace
   document.getElementById("average").innerHTML = average.rounded().formatted();
@@ -64,9 +66,12 @@ $.getJSON("writing-progress.json", function (data) {
   document.getElementById("days-remaining").innerHTML = PLANNED_COMPLETION.between(today).rounded().formatted();
   document.getElementById("days-writing").innerHTML = days_writing.formatted();
   document.getElementById("days-writing-variance-percent").innerHTML = ((days_lapsed - days_writing) * 100 / days_lapsed).rounded();
+}
 
-  var ctx = document.getElementById('weekly-progress').getContext('2d');
-  var options = {
+
+function generateChart() {
+  const ctx = document.getElementById('weekly-progress').getContext('2d');
+  const options = {
     options: {
       maintainAspectRatio: false,
       scales: { y: { ticks: { beginAtZero: true } } }
@@ -84,6 +89,26 @@ $.getJSON("writing-progress.json", function (data) {
     }
   }
   new Chart(ctx, options);
+}
+
+$.getJSON("writing-progress.json", function (data) {
+  uData = data["data"];
+  Object.keys(uData).map(function (key) {
+    var date = new Date(key + 'T06:20:30Z')
+    if (date < start_date) { start_date = date; }
+
+    if (uData[key]['wpd'] > 0 && today.between(date) < MAX_RANGE) {
+      var label = date.getMonday().toISOString().split('T')[0]
+      if (!(label in words)) { words[label] = 0 }
+      words[label] = words[label] + uData[key]['wpd'];
+      earned_value = earned_value + uData[key]['wpd'];
+      total_hours = total_hours + uData[key]['hours'];
+      hours.push(uData[key]['hours'])
+      days_writing = days_writing + 1;
+    }
+  })
+  calculateProgress()
+  generateChart()
 });
 
 Date.prototype.getWeek = function () {
@@ -116,8 +141,11 @@ Date.prototype.addDays = function (days) {
 Date.prototype.toString = function (days) {
   return this.toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
 }
-Number.prototype.rounded = function () {
-  return Math.round(this.valueOf());
+Number.prototype.rounded = function (length = 0) {
+  if (length > 0) {
+    return this.valueOf().toFixed(length);
+  }
+  return Math.round(this.valueOf(), length);
 }
 Number.prototype.formatted = function () {
   return this.toLocaleString("en-US")
