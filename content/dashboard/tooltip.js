75af968a740369b47544098d702b5bb750bb2408
Ben W <merovex@hey.com>
2023-04-09 08:33:35 -0400

    removing dashboard

--- content/dashboard/tooltip.js
+++
@@ -1,7 +0,0 @@
-function createTip(ev) {
-  const msg = this.dataset.date + ": " + this.dataset.count + " words"
-  document.getElementById("msg").innerHTML = msg
-}
-function cancelTip(ev) {
-  document.getElementById("msg").innerHTML = ''
-}
